c33a5144e96bf4bdc9394d33dadd171cc54252ef
redheadedrod <redheadedrod@hotmail.com>
2015-10-24 00:02:50 -0400

    Commit source

---
+++ NetTraffic.java
@@ -0,0 +1,15 @@
+package nettraffic;
+
+/**
+ * Created by Rodney on 10/23/2015.
+ */
+
+
+public class NetTraffic {
+    public static ProcessPacket pp;
+    public static void main (String args[]) {
+        pp = new ProcessPacket();
+        pp.PrintOut();
+    }
+
+}
---
+++ ProcessPacket.java
@@ -0,0 +1,14 @@
+package nettraffic;
+import org.jnetpcap.*;
+/**
+ * Created by Rodney on 10/23/2015.
+ */
+public class ProcessPacket {
+
+    public ProcessPacket() {
+
+    }
+    public void PrintOut() {
+        System.out.println("Test");
+    }
+}
