6f311422c7dff173bd3ce2767145b2cd05f66b88
redheadedrod <redheadedrod@hotmail.com>
2015-10-24 17:03:23 -0400

    Updated with working processfile routine. Need to figure how to process arraylist next

--- NetTraffic.java
+++ NetTraffic.java
@@ -3,11 +3,12 @@ package nettraffic;
 public class NetTraffic {
     public static ProcessPacket pp;
     public static void main (String args[]) {
-        String file = "C:\\Users\\Rodney\\SkyDrive\\School\\CIS 457 Datacom\\NetTraffic\\src\\nettraffic\\sample.pcap"; // May need to be changed to actual filename or pass on cl
         if (args.length>0)
-            file = args[0];
-        pp = new ProcessPacket(file);
-        pp.ProcessFile();
+            pp = new ProcessPacket(args[0]);
+        else
+            pp = new ProcessPacket();
+        //pp.ethernetIIPackets();
+        System.out.println("Number of records read in = " + Integer.toString(pp.length()));
     }
 
 }
